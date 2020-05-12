4e83ac4567df2c1b89043e7eb1f5a40a716bb1e4
霍二帅 <huoershuai@mobike.com>
2020-05-12 14:36:52 +0800

    db构造完成

---
+++ src/main/java/com/lucien/myspringboot/dao/BaseDao.java
@@ -0,0 +1,11 @@
+package com.lucien.myspringboot.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author huoershuai
+ * @Date 2020/5/12
+ */
+@Mapper
+public class BaseDao {
+}
---
+++ src/main/java/com/lucien/myspringboot/dao/entity/BaseDO.java
@@ -0,0 +1,17 @@
+package com.lucien.myspringboot.dao.entity;
+
+import lombok.Data;
+
+/**
+ * @Author huoershuai
+ * @Date 2020/5/12
+ */
+@Data
+public class BaseDO {
+
+    private String col1;
+
+    private String col2;
+
+    private String col3;
+}
